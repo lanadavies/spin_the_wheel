c2f9cdc20cb7d5e8ae24f1fd422720bfbcc826fe
lanadavies <allana.davies@outlook.com>
2025-05-25 16:59:18 +1000

    Updated assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import SpinnerWheel from './components/SpinnerWheel';
 import { ChakraProvider } from '@chakra-ui/react'
 function App() {
   return (
-    <div className="App">
+    <div className="App"
+    style={{
+      backgroundImage: "url('/book_club_background.png')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "100vh",
+      width: "100%",
+    }}
+    >
       <ChakraProvider>
         <SpinnerWheel />
       </ChakraProvider>
